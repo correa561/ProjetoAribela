9b20877447dcfcb59212552a194ce258155ec7fc
Corrêa <jovicorreabz@gmail.com>
2025-07-19 19:36:40 -0400

    fist commit

---
+++ src/main/java/correaDev/com/ProjetoAribela/ProjetoAribelaApplication.java
@@ -0,0 +1,13 @@
+package correaDev.com.ProjetoAribela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAribelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAribelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/correaDev/com/ProjetoAribela/ProjetoAribelaApplicationTests.java
@@ -0,0 +1,13 @@
+package correaDev.com.ProjetoAribela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAribelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
