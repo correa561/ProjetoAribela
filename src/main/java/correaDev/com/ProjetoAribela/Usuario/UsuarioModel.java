990fbef53c26546f16f94c41a2bb94af4a4c9d1f
Corrêa <jovicorreabz@gmail.com>
2025-07-23 20:59:12 -0400

    feat : Transformando a classe id do UsuarioModel em uma Entity! com JPA.

--- src/main/java/correaDev/com/ProjetoAribela/Usuario/UsuarioModel.java
+++ src/main/java/correaDev/com/ProjetoAribela/Usuario/UsuarioModel.java
@@ -1,5 +1,6 @@
 package correaDev.com.ProjetoAribela.Usuario;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_usuario_e_senha")
 
 public class UsuarioModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String nome;
     String email;
